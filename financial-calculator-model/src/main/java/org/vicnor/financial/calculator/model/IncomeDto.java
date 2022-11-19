5be7dc8572aeeca6166c40a019cb75c2e6c0160b
Victor Nordstrand <victor@reepay.com>
2022-11-19 19:17:13 +0100

    add createIncome method to IncomeController and validation to IncomeDto

--- financial-calculator-model/src/main/java/org/vicnor/financial/calculator/model/IncomeDto.java
+++ financial-calculator-model/src/main/java/org/vicnor/financial/calculator/model/IncomeDto.java
@@ -3,6 +3,11 @@ package org.vicnor.financial.calculator.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -11,8 +16,17 @@ import java.time.LocalDate;
 public class IncomeDto {
 
     private long id;
+
+    @NotBlank
     private String description;
+
+    @NotNull
+    @Future
     private LocalDate dueDate;
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
+    @Digits(integer = 3, fraction = 2)
     private BigDecimal amount;
 
 }
--- financial-calculator-service/src/main/java/org/vicnor/financial/calculator/controller/IncomeController.java
+++ financial-calculator-service/src/main/java/org/vicnor/financial/calculator/controller/IncomeController.java
@@ -1,4 +1,20 @@
 package org.vicnor.financial.calculator.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.vicnor.financial.calculator.model.IncomeDto;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/cashflow")
 public class IncomeController {
+
+    @PostMapping("/income")
+    public IncomeDto createIncome(@RequestBody @Valid IncomeDto income) {
+        return income;
+    }
+
 }
