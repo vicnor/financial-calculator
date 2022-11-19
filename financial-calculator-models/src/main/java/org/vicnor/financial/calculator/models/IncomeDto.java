396e30f6af5823f96c2ebb81610c8c514f67df34
Victor Nordstrand <victor@reepay.com>
2022-11-19 10:55:54 +0100

    add module financial-calculator-models with class IncomeDto

---
+++ financial-calculator-models/src/main/java/org/vicnor/financial/calculator/models/IncomeDto.java
@@ -0,0 +1,18 @@
+package org.vicnor.financial.calculator.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class IncomeDto {
+
+    private long id;
+    private String description;
+    private LocalDate dueDate;
+    private BigDecimal amount;
+
+}
